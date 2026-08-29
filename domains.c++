1041e64a8071638ca8dfe8ea3373d543022ad737
Justin <packma007@gmail.com>
2026-08-29 14:50:59 +0900

    restart

--- domains.c++
+++ domains.c++
@@ -3,22 +3,4 @@
 #include <string>
 
 namespace domains{
-    int student::getIp(){
-        return ip;
-    }
-    std::string student::getName(){
-        return name;
-    }
-    std::string student::getPassword(){
-        return password;
-    }
-    void student::setId(int idVal){
-        ip = idVal;
-    }
-    void student::setName(std::string nameVal){
-        name = nameVal;
-    }
-    void student::setPassword(std::string passVal){
-        password = passVal;
-    }
 }
--- domains.h
+++ domains.h
@@ -1,31 +1,10 @@
 #ifndef DOMAINS
 #define DOMAINS
 #include <string>
+#include <array>
+#include <vector>
 namespace domains {
-    class student{
-        private:
-        int ip;
-        std::string name;
-        std::string password;
-        public:
-        //constructor
-        student():ip(0), name("익명1"), password("password"){}
-        //getters, setters
-        int getIp();
-        std::string getName();
-        std::string getPassword();
-        void setId(int idVal);
-        void setName(std::string nameVal);
-        void setPassword(std::string passVal);
-        
-    };
-    class weeklyMenu{
-        private:
-        public:
-    };
-    class MenuSet{};
-    class Order{};
-    class PickupSpot{};
+    
 
 }
 
